b4ac6402ca43ca29c597ca7f21eb97681d767ea5
langhuansanren <1187018133@qq.com>
2015-04-11 16:43:25 +0800

    second version

--- source/DataStruct.cpp
+++ source/DataStruct.cpp
@@ -2,16 +2,84 @@
 //
 
 #include "stdafx.h"
-
+#define MAXLEN 100;
+/*********递归求最大公约数**********/
 int gcd(int m, int n){
 	if (n == 0) return m;
 	return gcd(n, m%n);
 }
 
+struct Stack{
+	char* data;
+	int top;
+	int Maxlen;
+};
+
+void initS(Stack& s){
+	s.data =(char*) malloc(100 * sizeof(char));
+	s.top = 0;
+	s.Maxlen = MAXLEN;
+}
+bool emptyS(Stack& s){
+	return s.top == 0 ? 1 : 0;
+}
+bool fullS(Stack& s){
+	return (s.top==s.Maxlen-1)?1 : 0;
+}
+void push(Stack &s, char c){
+	if (fullS(s)) return;
+	s.data[s.top] = c;
+	s.top=(s.top+1)%s.Maxlen;
+}
+
+void pop(Stack &s, char& c){
+	if (emptyS(s)) return;
+	s.top = (s.top - 1) % s.Maxlen;
+	c=s.data[s.top] ;
+}
+bool march(char a, char c){
+	if ((a == '(' && c == ')') || (a == ')' && c == '('))
+		return true;
+	if ((a == '{' && c == '}') || (a == '}' && c == '{'))
+		return true;
+	if ((a == ']' && c == '[') || (a == '[' && c == ']'))
+		return true;
+	return false;
+}
+
+bool kuohaoMarch(char* str){
+	char *tmp = str;
+	//Stack* s=(Stack*)malloc(sizeof(Stack));
+	Stack s;
+	initS(s);
+
+	while (*tmp != '\0'){
+		if (*tmp == '(' || *tmp == '{' || *tmp == '[')
+			push(s, *tmp);
+		else if (*tmp == ')' || *tmp == '}' || *tmp == ']'){
+			char t=' ';
+			pop(s, t);
+			if (!march(*tmp, t)){
+				return false;
+			}
+		}
+		tmp++;
+	}
+
+	if (emptyS(s))return true;
+	return false;
+
+}
+
+
 int main()
 {
-	int s=gcd(12, 8);
-	printf("最大公约数是：%d\n", s);
-	return 0;
+	bool s =kuohaoMarch("{[(a+[]b)[111-22()]^(c-d)]}");
+	if (s){
+		printf("正确匹配！\n");
+	}
+	else{
+		printf("匹配错误！\n");
+	}
 }
 
